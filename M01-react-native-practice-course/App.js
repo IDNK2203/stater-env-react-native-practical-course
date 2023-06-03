16bb525b04633b93c702e1b7f0c9c2e4ed8d1359
idnk <efeidukpaye@gmail.com>
2023-06-03 17:02:00 +0100

    completed module 1

--- M01-react-native-practice-course/App.js
+++ M01-react-native-practice-course/App.js
@@ -4,24 +4,16 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={styles.texting}>I'm gom show you how great I am.</Text>
+      <Text>Open up App.js to start working on your app!2</Text>
       <StatusBar style='auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  texting: {
-    // flex: 1,
-    backgroundColor: "purple",
-    // fontSize: "32px",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#aff",
     alignItems: "center",
     justifyContent: "center",
   },
