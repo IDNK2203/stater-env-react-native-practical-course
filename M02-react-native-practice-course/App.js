939b27aec86c9173fa5ae6ebcc374cf23d13a143
idnk <efeidukpaye@gmail.com>
2023-06-09 11:00:43 +0100

    L21 styling with the flex property

--- M02-react-native-practice-course/App.js
+++ M02-react-native-practice-course/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
+      <View>
+        <Text style={styles.heading}>Goal Setter🎯!</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='set a goal' />
+        <Button color='#0dcddf' title='Tap For Luck' />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text> List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 30,
     flex: 1,
-    backgroundColor: "#ffd",
+  },
+  heading: {
+    textAlign: "center",
+    fontSize: 24,
+    margin: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    marginTop: 16,
+    marginBottom: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    paddingBottom: 20,
+    borderBottomWidth: 2,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    height: 45,
+    marginRight: 8,
+    width: "65%",
+    padding: 8,
+    borderColor: "#ccc",
+    borderWidth: 2,
+  },
+  goalsContainer: {
+    flex: 6,
   },
 });
