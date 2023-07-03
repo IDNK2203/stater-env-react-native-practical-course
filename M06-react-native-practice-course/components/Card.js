3505b2c1f6655e173bed8495ed51b5cbed81167a
idnk <efeidukpaye@gmail.com>
2023-07-03 23:17:22 +0100

    L91 (setting up meal application)

---
+++ M06-react-native-practice-course/components/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import Colors from "../utils/colors";
+
+const Card = ({ children, style = {} }) => {
+  return <View style={[styles.conatainer, style]}>{children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  conatainer: {
+    // backgroundColor: Colors.primary,
+    // backgroundColor: Platform.OS === "android" ? "orange" : "grey",
+    // backgroundColor: Platform.select({ ios: "orange", android: "grey" }),
+    backgroundColor: "grey",
+    marginVertical: 16,
+    padding: 16,
+    borderRadius: 8,
+    elevation: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
+    alignItems: "center",
+  },
+});
