76f5ba5dc9f43d493f713211ffc92cdf0893fb92
idnk <efeidukpaye@gmail.com>
2023-07-12 21:28:36 +0100

    L108 - 109 (setting up a drawer navigation)

--- M06-react-native-practice-course/App.js
+++ M06-react-native-practice-course/App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import CategoryListing from "./screens/CategoryListing";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Category from "./screens/Category";
 import Meal from "./screens/Meal";
 import Colors from "./utils/colors";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style='light' />
-      <Stack.Navigator
+      <Drawer.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: Colors.bg },
           contentStyle: { backgroundColor: Colors.bg },
@@ -21,16 +24,16 @@ export default function App() {
         }}
         initialRouteName='Meal Categories'
       >
-        <Stack.Screen
+        <Drawer.Screen
           name='Meal Categories'
           options={{
             title: "Meal Categories",
           }}
           component={CategoryListing}
         />
-        <Stack.Screen name='Meal Category' component={Category} />
-        <Stack.Screen name='Meal' component={Meal} />
-      </Stack.Navigator>
+        <Drawer.Screen name='Meal Category' component={Category} />
+        <Drawer.Screen name='Meal' component={Meal} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
