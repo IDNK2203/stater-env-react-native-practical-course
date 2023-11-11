56b294b59b48e108bce8746aacf713a72a147044
idnk <efeidukpaye@gmail.com>
2023-11-11 12:13:59 +0100

    M12 L204-205

--- M12-react-native-practice-course/screens/Map.js
+++ M12-react-native-practice-course/screens/Map.js
@@ -1,7 +1,39 @@
-import MapView from "react-native-maps";
-import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { Alert, StyleSheet, View } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import IconButton from "../components/app-ui/IconButton";
 
 const Map = () => {
+  let [selectedLocation, setSelectedLocation] = useState(null);
+  const navigation = useNavigation();
+
+  const mapViewHandler = (e) => {
+    let { latitude, longitude } = e.nativeEvent.coordinate;
+    setSelectedLocation({ latitude, longitude });
+  };
+
+  const saveLocation = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert("Error Saving", "Pick a location");
+      return;
+    }
+    navigation.navigate("AddPlaces", { selectedLocation });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          color={tintColor}
+          type={"save"}
+          size={24}
+          onPress={saveLocation}
+        />
+      ),
+    });
+  }, [navigation, selectedLocation]);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -12,7 +44,17 @@ const Map = () => {
           longitudeDelta: 0.0421,
         }}
         style={styles.map}
-      />
+        onPress={mapViewHandler}
+      >
+        {selectedLocation && (
+          <Marker
+            draggable
+            onDragEnd={mapViewHandler}
+            coordinate={selectedLocation}
+            title='The Location'
+          />
+        )}
+      </MapView>
     </View>
   );
 };
